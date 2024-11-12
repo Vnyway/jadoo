2b6ba7655a4b6fe5fce43ad60c0ca9339d33d10d
Vnywvy <bohdan05zbr@gmail.com>
2024-11-12 13:51:46 +0200

    start constants and folder structure

---
+++ src/constants/index.js
@@ -0,0 +1,84 @@
+export const navLinks = [
+  {
+    id: 1,
+    title: "Services",
+    ref: "#services",
+  },
+  {
+    id: 2,
+    title: "Desitnations",
+    ref: "#desitnations",
+  },
+  {
+    id: 3,
+    title: "Booking",
+    ref: "#booking",
+  },
+  {
+    id: 4,
+    title: "Testimonials",
+    ref: "#testimonials",
+  },
+  {
+    id: 5,
+    title: "Subscribe",
+    ref: "#subscribe",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    title: "Calculated Weather",
+    description: "Built Wicket longer admire do barton vanity itself do in it.",
+    shadowPosition: "bottom-right",
+    image: "weather.svg",
+  },
+  {
+    id: 2,
+    title: "Best Flights",
+    description: "Engrossed listening. Park gate sell they west hard for the.",
+    shadowPosition: "top-left",
+    image: "flights.svg",
+  },
+  {
+    id: 3,
+    title: "Local Events",
+    description:
+      "Barton vanity itself do in it. Preferd to men it engrossed listening.",
+    shadowPosition: "top-right",
+    image: "events.svg",
+  },
+  {
+    id: 4,
+    title: "Customization",
+    description:
+      "We deliver outsourced aviation services for military customers",
+    shadowPosition: "bottom-right",
+    image: "customization.svg",
+  },
+];
+
+export const destinations = [
+  {
+    id: 1,
+    image: "rome.svg",
+    location: "Rome, Italty",
+    price: 5420,
+    time: 10,
+  },
+  {
+    id: 2,
+    image: "london.svg",
+    location: "London, UK",
+    price: 4200,
+    time: 12,
+  },
+  {
+    id: 3,
+    image: "europe.svg",
+    location: "Full Europe",
+    price: 15000,
+    time: 28,
+  },
+];
