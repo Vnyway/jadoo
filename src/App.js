9bc864feb4e4a311ae06d9db0b71ee368bf1e8d7
Vnywvy <bohdan05zbr@gmail.com>
2024-11-15 14:06:21 +0200

    hero

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import "./styles/main.scss";
+import React from "react";
 
 const App = () => {
   return (
-    <>
+    <React.Fragment className="page">
+      <img
+        src="./images/general/decore.png"
+        alt="decore"
+        className="background-image"
+      />
       <Header />
-      <main>
+      <main className="main__wrapper">
         <Hero />
         <Services />
         <Destinations />
       </main>
-    </>
+    </React.Fragment>
   );
 };
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Hero = () => {
+  const [hovered, setHovered] = useState(false);
   return (
-    <section>
-      <h1>Hero</h1>
+    <section className="hero">
+      <div className="hero__content">
+        <h4>Best Destinations around the world</h4>
+        <h1>Travel, enjoy and live a new and full life</h1>
+        <p>
+          Built Wicket longer admire do barton vanity itself do in it. Preferred
+          to sportsmen it engrossed listening. Park gate sell they west hard for
+          the.
+        </p>
+        <div className="hero__content__buttons">
+          <button className="hero__content__more-button">Find out more</button>
+          <button
+            className="hero__content__play-button"
+            onMouseOver={() => setHovered(true)}
+            onMouseOut={() => setHovered(false)}>
+            <div>
+              <svg
+                width="12"
+                height="13"
+                viewBox="0 0 12 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 6.5L0.749999 12.9952L0.75 0.00480889L12 6.5Z"
+                  style={{ transition: "all .3s ease-out" }}
+                  fill={hovered ? "#DF6951" : "white"}
+                />
+              </svg>
+            </div>
+            <span>Play Demo</span>
+          </button>
+        </div>
+      </div>
+      <div className="hero__image">
+        <img src="./images/general/traveler.png" alt="traveler" />
+      </div>
     </section>
   );
 };
