1e9039d3e0feb4e1b4b5151928ead5edeefd0340
Vnywvy <bohdan05zbr@gmail.com>
2024-11-15 14:06:03 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,14 +11,20 @@ const Header = () => {
         <div className="header__navigation">
           <ul className="header__links">
             {navLinks.map((link) => (
-              <li key={link.id} className="header__link-item">
+              <li
+                key={link.id}
+                className="header__link-item"
+                style={{ animationDelay: `${(link.id + 1) * 0.3}s` }}>
                 <a href={link.ref} className="header__link">
                   {link.title}
                 </a>
               </li>
             ))}
           </ul>
-          <a className="header__subscribe" href="#subscribe">
+          <a
+            className="header__subscribe"
+            href="#subscribe"
+            style={{ animationDelay: `${(navLinks.length + 2) * 0.3}s` }}>
             Subscribe
           </a>
         </div>
